77c17906c9068b6d3bd1df7a7deea456e2c33925
Davis Keiner Pérez Guzmán <davisperezg@gmail.com>
2022-01-26 02:28:33 -0500

    env agregado

--- src/lib/strategies/jwt.strategies.ts
+++ src/lib/strategies/jwt.strategies.ts
@@ -2,7 +2,6 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { AuthService } from 'src/auth/services/auth.service';
-import { jwtConstants } from '../const/consts';
 
 interface JWType {
   userId: string;
@@ -14,7 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: jwtConstants.secret,
+      secretOrKey: process.env.JWT_SECRET_KEY,
     });
   }
 
