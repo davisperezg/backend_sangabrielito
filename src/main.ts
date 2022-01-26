af287c6657083ddb13d39500e05b3969f1aa7a4f
Davis Keiner Pérez Guzmán <davisperezg@gmail.com>
2022-01-26 01:25:18 -0500

    cambio de puerto

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: process.env.URL_FRONTEND_ORIGIN });
-  await app.listen(3000);
+  await app.listen(process.env.PORT_APP);
 }
 bootstrap();
